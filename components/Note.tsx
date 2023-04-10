284f877cb533192b3852894ee48b183a447191a4
fglukhov <f.glukhov@gmail.com>
2023-04-10 17:45:38 +0400

    Notes list layout fixed

--- components/Note.tsx
+++ components/Note.tsx
@@ -16,10 +16,22 @@ export type NoteProps = {
 const Note: React.FC<{ note: NoteProps }> = ({ note }) => {
   const authorName = note.author ? note.author.name : "Unknown author";
   return (
-    <div className="notes-item" onClick={() => Router.push("/n/[id]", `/n/${note.id}`)}>
-      <div className="notes-item-title">{note.title}</div>
+    <div className="notes-list-item" onClick={() => Router.push("/n/[id]", `/n/${note.id}`)}>
+      <div className="notes-list-item-title">{note.title}</div>
       <style jsx>{`
-        
+				.notes-list-item {
+					background: white;
+					transition: box-shadow 0.1s ease-in;
+					padding: 10px 20px;
+				}
+
+				.notes-list-item:hover {
+					box-shadow: 1px 1px 3px #aaa;
+				}
+
+				.notes-list-item + .notes-list-item {
+					margin-top: 1px;
+				}
       `}</style>
     </div>
   );
