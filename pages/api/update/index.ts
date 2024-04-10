ce8f525ca1555b08f53143d4f0ca7e0a475561cd
fglukhov <f.glukhov@gmail.com>
2024-04-10 09:21:56 +0400

    nav on collapsed notes fixed

--- pages/api/update/index.ts
+++ pages/api/update/index.ts
@@ -4,7 +4,7 @@ import { getSession } from 'next-auth/react';
 import prisma from '../../../lib/prisma';
 
 // POST /api/note
-// Required fields in body: title
+// Required fields in body: titlenext
 // Optional fields in body: content
 
 export const config = {
@@ -38,6 +38,8 @@ export default async function handle(req, res) {
 				id: id,
 				sort: sort,
 				parentId: parentId,
+				complete: complete,
+				collapsed: collapsed,
 				title: title,
 				author: { connect: { email: session?.user?.email } },
 			}
