61566b3c08bd9c197f331e124c29f3f4c85615bd
fglukhov <f.glukhov@gmail.com>
2026-03-26 14:21:28 +0400

    md editor 2

--- components/MarkdownNoteEditor.tsx
+++ components/MarkdownNoteEditor.tsx
@@ -1,7 +1,10 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import dynamic from 'next/dynamic';
 
-const MDEditor = dynamic(() => import('@uiw/react-md-editor'), { ssr: false });
+const MarkdownNoteEditorClient = dynamic(
+  () => import('./MarkdownNoteEditorClient'),
+  { ssr: false },
+);
 
 type MarkdownNoteEditorProps = {
   value: string;
@@ -10,25 +13,4 @@ type MarkdownNoteEditorProps = {
   placeholder?: string;
 };
 
-const MarkdownNoteEditor: React.FC<MarkdownNoteEditorProps> = ({
-  value,
-  onChange,
-  autoFocus,
-  placeholder,
-}) => {
-  const editorValue = useMemo(() => value ?? '', [value]);
-
-  return (
-    <MDEditor
-      value={editorValue}
-      onChange={(val) => onChange(val ?? '')}
-      autoFocus={autoFocus}
-      textareaProps={{ autoFocus, placeholder }}
-      hideToolbar
-      preview="edit"
-      height={250}
-    />
-  );
-};
-
-export default MarkdownNoteEditor;
+export default MarkdownNoteEditorClient as React.FC<MarkdownNoteEditorProps>;
