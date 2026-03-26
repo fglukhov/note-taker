68b8d023195698181513f98604eff0bd6f8cf3b3
fglukhov <f.glukhov@gmail.com>
2026-03-26 13:35:53 +0400

    hints component

--- components/NotesList.tsx
+++ components/NotesList.tsx
@@ -4,6 +4,7 @@ import { NotesProvider } from '@/components/NotesContext';
 import { NotesListItemProps } from '@/components/NotesListItem';
 import { useKeyPress } from '@/lib/useKeyPress';
 import { getFamily, removeFamily } from '@/lib/notesTree';
+import NotesHotkeysHints from '@/components/NotesHotkeysHints';
 import styles from '@/components/NotesList.module.scss';
 import Router from 'next/router';
 
@@ -933,86 +934,7 @@ const NotesList: React.FC<Props> = (props) => {
         </div>
       </div>
       <div className={`col ${styles.notes_list_col_2}`}>
-        <div className={styles.sidebar_hints}>
-          <div className={styles.hints_list}>
-            <div className={styles.hints_item}>
-              <div className={styles.hints_item_key}>
-                <div className={styles.key}>Enter</div>
-              </div>
-              <div className={styles.hints_item_descr}>Add note</div>
-            </div>
-            <div className={styles.hints_item}>
-              <div className={styles.hints_item_key}>
-                <div className={styles.key}>Alt + Enter</div>
-              </div>
-              <div className={styles.hints_item_descr}>Add above</div>
-            </div>
-            <div className={styles.hints_item}>
-              <div className={styles.hints_item_key}>
-                <div className={styles.key}>Shift + Enter</div>
-              </div>
-              <div className={styles.hints_item_descr}>Add a sub-item</div>
-            </div>
-            <div className={styles.hints_item}>
-              <div className={styles.hints_item_key}>
-                <div className={styles.key}>ee</div>
-              </div>
-              <div className={styles.hints_item_descr}>Edit title</div>
-            </div>
-            <div className={styles.hints_item}>
-              <div className={styles.hints_item_key}>
-                <div className={styles.key}>nn</div>
-              </div>
-              <div className={styles.hints_item_descr}>Open note</div>
-            </div>
-            <div className={styles.hints_item}>
-              <div className={styles.hints_item_key}>
-                <div className={styles.key}>↑</div>
-                <div className={styles.key}>↓</div>
-              </div>
-              <div className={styles.hints_item_descr}>Navigate</div>
-            </div>
-            <div className={styles.hints_item}>
-              <div className={styles.hints_item_key}>
-                <div className={styles.key}>←</div>
-                <div className={styles.key}>→</div>
-              </div>
-              <div className={styles.hints_item_descr}>Collapse/expand</div>
-            </div>
-            <div className={styles.hints_item}>
-              <div className={styles.hints_item_key}>
-                <div className={styles.key}>Ctrl + →</div>
-              </div>
-              <div className={styles.hints_item_descr}>Indent</div>
-            </div>
-            <div className={styles.hints_item}>
-              <div className={styles.hints_item_key}>
-                <div className={styles.key}>Ctrl + ←</div>
-              </div>
-              <div className={styles.hints_item_descr}>Outdent</div>
-            </div>
-            <div className={styles.hints_item}>
-              <div className={styles.hints_item_key}>
-                <div className={styles.key}>Ctrl + ↑</div>
-                <div className={styles.key}>Ctrl + ↓</div>
-              </div>
-              <div className={styles.hints_item_descr}>Reorder notes</div>
-            </div>
-
-            <div className={styles.hints_item}>
-              <div className={styles.hints_item_key}>
-                <div className={styles.key}>Spacebar</div>
-              </div>
-              <div className={styles.hints_item_descr}>Complete/reopen</div>
-            </div>
-            <div className={styles.hints_item}>
-              <div className={styles.hints_item_key}>
-                <div className={styles.key}>Del</div>
-              </div>
-              <div className={styles.hints_item_descr}>Delete</div>
-            </div>
-          </div>
-        </div>
+        <NotesHotkeysHints />
       </div>
     </div>
   );
