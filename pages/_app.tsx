1438611f894a1e673b101d9b3a6e51fc22d933bf
fglukhov <f.glukhov@gmail.com>
2026-04-18 15:58:10 +0400

    tailwind, Button component, notes sync from modal updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { AppProps } from 'next/app';
 import { Analytics } from '@vercel/analytics/next';
 import '../assets/scss/vars.scss';
 import '../assets/scss/global.scss';
+import '@/assets/css/app.css';
 import '@milkdown/crepe/theme/common/style.css';
 import '@milkdown/crepe/theme/frame.css';
 
