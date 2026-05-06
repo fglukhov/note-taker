9ddcffba1f491992a12364f350d2588f34dbe07e
fglukhov <f.glukhov@gmail.com>
2026-05-06 09:53:41 +0400

    typography & mobile styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,18 @@ import Head from 'next/head';
 import { SessionProvider } from 'next-auth/react';
 import { AppProps } from 'next/app';
 import { Analytics } from '@vercel/analytics/next';
+import { JetBrains_Mono } from 'next/font/google';
+import '@milkdown/crepe/theme/common/style.css';
+import '@milkdown/crepe/theme/frame.css';
 import '../assets/scss/vars.scss';
 import '../assets/scss/global.scss';
 import '@/assets/css/app.css';
-import '@milkdown/crepe/theme/common/style.css';
-import '@milkdown/crepe/theme/frame.css';
+
+const jetBrainsMono = JetBrains_Mono({
+  subsets: ['latin'],
+  variable: '--font-jetbrains-mono',
+  display: 'swap',
+});
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
@@ -15,8 +22,10 @@ const App = ({ Component, pageProps }: AppProps) => {
         <title>RootNote – Tree notes & tasks, keyboard-first</title>
       </Head>
       <SessionProvider session={pageProps.session}>
-        <Component {...pageProps} />
-        <Analytics />
+        <div className={jetBrainsMono.variable}>
+          <Component {...pageProps} />
+          <Analytics />
+        </div>
       </SessionProvider>
     </>
   );
